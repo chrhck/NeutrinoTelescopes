0e3f62ec35665ecefda6633809f55a670fa39de8
Christian Haack <christian.haack@tum.de>
2022-10-12 13:18:44 +0200

    remove norm flow include for now

--- src/NeutrinoTelescopes.jl
+++ src/NeutrinoTelescopes.jl
@@ -12,7 +12,6 @@ include("photon_propagation/PhotonProp.jl")
 include("pmt_frontend/PMTFrontEnd.jl")
 include("event_generation/EventGeneration.jl")
 
-include("surrogate_model/normalizing_flow.jl")
 
 @reexport using .NormalizingFlow
 @reexport using .PhotonPropagation
