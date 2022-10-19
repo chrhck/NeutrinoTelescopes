846a06adae365bd1c40d9577d94d65ad473907e0
Christian Haack <christian.haack@tum.de>
2022-10-19 15:33:32 +0200

    bugfix

--- scripts/photon_tables.jl
+++ scripts/photon_tables.jl
@@ -60,7 +60,112 @@ end
 parsed_args = parse_args(ARGS, s)
 
 
-function run_sim(parsed_args)
+function run_sim(energy, distance, dir_costheta, dir_phi, target, spectrum, medium)
+    sim_attrs = Dict(
+        "energy" => energy,
+        "distance" => distance,
+        "dir_costheta" => dir_costheta,
+        "dir_phi" => dir_phi)
+
+
+    direction::SVector{3,Float32} = sph_to_cart(acos(dir_costheta), dir_phi)
+
+    ppos = @SVector[0.0f0, 0.0f0, distance]
+    particle = Particle(
+        ppos,
+        direction,
+        0.0f0,
+        Float32(energy),
+        PEMinus
+    )
+
+    oversample = 1.0
+    photons = nothing
+    prop_source = nothing
+
+    while true
+        prop_source = ExtendedCherenkovEmitter(particle, medium, (300.0f0, 800.0f0); oversample=oversample)
+        if prop_source.photons > 1E13
+            println("More than 1E13 photons, skipping")
+            break
+        end
+        photons = propagate_photons(prop_source, target, medium, spectrum)
+
+        println(format("Distance {:.1f} Photons: {:d} Hits: {:d}", distance, prop_source.photons, nrow(photons)))
+        if nrow(photons) > 100
+            break
+        end
+        oversample *= 10
+    end
+
+    if isnothing(photons)
+        return nothing
+    end
+
+    calc_time_residual!(photons, prop_source, target, medium)
+    transform!(photons, :position => (col -> hcat(col...)') => [:pos_x, :pos_y, :pos_z])
+    calc_total_weight!(photons, target, medium)
+    photons[!, :total_weight] ./= oversample
+
+    save_hdf!(
+        parsed_args["output"],
+        "photons",
+        Matrix{Float64}(photons[:, [:tres, :pos_x, :pos_y, :pos_z, :total_weight]]),
+        sim_attrs)
+
+
+    @progress "Resampling" for j in 1:100
+        #=
+        PMT positions are defined in a standard upright coordinate system centeres at the module
+        Sample a random rotation matrix and rotate the pmts on the module accordingly.
+        =#
+        orientation = rand(RotMatrix3)
+        hits = make_hits_from_photons(photons, target, orientation)
+
+        if nrow(hits) < 10
+            continue
+        end
+
+
+        hits = resample_simulation(hits; per_pmt=true)
+        #hits[!, :total_weight] ./= oversample
+        if nrow(hits) == 0
+            continue
+        end
+
+        #=
+        Rotating the module (active rotation) is equivalent to rotating the coordinate system
+        (passive rotation). Hence rotate the position and the direction of the light source with the
+        inverse rotation matrix to obtain a description in which the module axis is again aligned with ez
+        =#
+        direction_rot = orientation' * direction
+        position_rot = orientation' * ppos
+
+        position_rot_normed = position_rot ./ norm(position_rot)
+        dir_theta, dir_phi = cart_to_sph(direction_rot)
+        pos_theta, pos_phi = cart_to_sph(position_rot_normed)
+
+        # Sanity check:
+
+        if !((dot(ppos / norm(ppos), direction) ≈ dot(position_rot_normed, direction_rot)))
+            error("Relative angle not perseved")
+        end
+
+        sim_attrs["dir_theta"] = dir_theta
+        sim_attrs["dir_phi"] = dir_phi
+        sim_attrs["pos_theta"] = pos_theta
+        sim_attrs["pos_phi"] = pos_phi
+
+        save_hdf!(
+            parsed_args["output"],
+            "pmt_hits",
+            Matrix{Float64}(hits[:, [:tres, :pmt_id]]),
+            sim_attrs)
+    end
+end
+
+
+function run_sims(parsed_args)
 
     #=
     parsed_args = Dict("n_sims"=>1, "n_skip"=>0)
@@ -68,10 +173,14 @@ function run_sim(parsed_args)
     medium = make_cascadia_medium_properties(0.99f0)
     pmt_area = Float32((75e-3 / 2)^2 * π)
     target_radius = 0.21f0
-
+    target = MultiPMTDetector(
+        @SVector[0.0f0, 0.0f0, 0.0f0],
+        target_radius,
+        pmt_area,
+        make_pom_pmt_coordinates(Float32)
+    )
     spectrum = CherenkovSpectrum((300.0f0, 800.0f0), 30, medium)
 
-    oversample = 1.0
 
     n_sims = parsed_args["n_sims"]
     n_skip = parsed_args["n_skip"]
@@ -92,114 +201,8 @@ function run_sim(parsed_args)
         dir_costheta = pars[3]
         dir_phi = pars[4]
 
-        sim_attrs = Dict(
-            "energy" => energy,
-            "distance" => distance,
-            "dir_costheta" => dir_costheta,
-            "dir_phi" => dir_phi)
-
-        target = MultiPMTDetector(
-            @SVector[0.0f0, 0.0f0, 0.0f0],
-            target_radius,
-            pmt_area,
-            make_pom_pmt_coordinates(Float32)
-        )
-
-        direction::SVector{3,Float32} = sph_to_cart(acos(dir_costheta), dir_phi)
-
-        ppos = @SVector[0.0f0, 0.0f0, distance]
-        particle = Particle(
-            ppos,
-            direction,
-            0.0f0,
-            Float32(energy),
-            PEMinus
-        )
-
-        oversample = 1.0
-        photons = nothing
-        prop_source = nothing
-
-        while true
-            prop_source = ExtendedCherenkovEmitter(particle, medium, (300.0f0, 800.0f0); oversample=oversample)
-            if prop_source.photons > 1E13
-                println("More than 1E13 photons, skipping")
-                break
-            end
-            photons = propagate_photons(prop_source, target, medium, spectrum)
-
-            println(format("Distance {:.1f} Photons: {:d} Hits: {:d}", distance, prop_source.photons, nrow(photons)))
-            if nrow(photons) > 100
-                break
-            end
-            oversample *= 10
-        end
-
-        if isnothing(photons)
-            continue
-        end
-
-        calc_time_residual!(photons, prop_source, target, medium)
-        transform!(photons, :position => (col -> hcat(col...)') => AsTable)
-        calc_total_weight!(photons, target, medium)
-        photons[!, :total_weight] ./= oversample
-
-        save_hdf!(
-            parsed_args["output"],
-            "photons",
-            Matrix{Float64}(photons[:, [:tres, :pos_x, :pos_y, :pos_z, :total_weight]]),
-            sim_attrs)
-
-
-        @progress "Resampling" for j in 1:100
-            #=
-            PMT positions are defined in a standard upright coordinate system centeres at the module
-            Sample a random rotation matrix and rotate the pmts on the module accordingly.
-            =#
-            orientation = rand(RotMatrix3)
-            hits = make_hits_from_photons(photons, target, orientation)
-
-            if nrow(hits) < 10
-                continue
-            end
-
-
-            hits = resample_simulation(hits; per_pmt=true)
-            #hits[!, :total_weight] ./= oversample
-            if nrow(hits) == 0
-                continue
-            end
-
-            #=
-            Rotating the module (active rotation) is equivalent to rotating the coordinate system
-            (passive rotation). Hence rotate the position and the direction of the light source with the
-            inverse rotation matrix to obtain a description in which the module axis is again aligned with ez
-            =#
-            direction_rot = orientation' * direction
-            position_rot = orientation' * ppos
-
-            position_rot_normed = position_rot ./ norm(position_rot)
-            dir_theta, dir_phi = cart_to_sph(direction_rot)
-            pos_theta, pos_phi = cart_to_sph(position_rot_normed)
-
-            # Sanity check:
-
-            if !((dot(ppos / norm(ppos), direction) ≈ dot(position_rot_normed, direction_rot)))
-                error("Relative angle not perseved")
-            end
-
-            sim_attrs["dir_theta"] = dir_theta
-            sim_attrs["dir_phi"] = dir_phi
-            sim_attrs["pos_theta"] = pos_theta
-            sim_attrs["pos_phi"] = pos_phi
-
-            save_hdf!(
-                parsed_args["output"],
-                "pmt_hits",
-                Matrix{Float64}(hits[:, [:tres, :pmt_id]]),
-                sim_attrs)
-        end
+        run_sim(energy, distance, dir_costheta, dir_phi, target, spectrum, medium)
     end
 end
 
-run_sim(parsed_args)
+run_sims(parsed_args)
