dd275fea306537c5432b7ddc8d692a023538758b
Christian Haack <christian.haack@tum.de>
2022-11-24 18:36:51 +0100

    updates

--- notebooks/julia_norm_flow.jl
+++ notebooks/julia_norm_flow.jl
@@ -16,53 +16,171 @@ using AutoMLPipeline
 using NeutrinoTelescopes
 using TensorBoardLogger
 using Logging
+using SpecialFunctions
 
 
 device = cpu
 
 
-fname = joinpath(@__DIR__, "../assets/photon_table_extended_2.hd5")
-fid = h5open(fname, "r")
-
-rng = MersenneTwister(31338)
-datasets = shuffle(rng, keys(fid["pmt_hits"]))
-nsel = 500
-
-function create_pmt_table(grp)
+function create_pmt_table(grp, limit=true)
     hits = DataFrame(grp[:, :], [:tres, :pmt_id])
     for (k, v) in attrs(grp)
         hits[!, k] .= v
     end
+
+    hits[!, :nhits] .= nrow(hits)
+
+    if limit && (nrow(hits) > 200)
+        hits = hits[1:200, :]
+    end
+
     return hits
 end
 
 
-all_hits = []
-for grpn in datasets[1:nsel]
-    grp = fid["pmt_hits"][grpn]
-    hits = create_pmt_table(grp)
-    push!(all_hits, hits)
-end
-
-
-function preproc(df)
-    tres = df[:, :tres]
-
-    df[!, :pmt_id] = categorical(df[:, :pmt_id], levels=1:16)
+function preproc_labels(df)
+    
     df[!, :log_distance] = log.(df[:, :distance])
     df[!, :log_energy] = log.(df[:, :energy])
     dir_cart = DataFrame(reduce(hcat, sph_to_cart.(df[:, :dir_theta], df[:, :dir_phi]))', [:dir_x, :dir_y, :dir_z])
     pos_cart = DataFrame(reduce(hcat, sph_to_cart.(df[:, :pos_theta], df[:, :pos_phi]))', [:pos_x, :pos_y, :pos_z])
 
-    feat = [:pmt_id, :log_distance, :log_energy]
+    if "pmt_id" in names(df)  
+        df[!, :pmt_id] = categorical(df[:, :pmt_id], levels=1:16)
+        feat = [:pmt_id, :log_distance, :log_energy]
+    else
+        feat = [:log_distance, :log_energy]
+    end
+
     cond_labels = hcat(df[:, feat], dir_cart, pos_cart)
-    return tres, cond_labels
+    return cond_labels
 end
 
 
-tres, cond_labels = preproc(reduce(vcat, all_hits))
+function read_hdf(fname, nsel=500, rng=nothing)
 
-extrema(tres)
+    all_hits = []
+    attr_dicts = []
+    h5open(fname, "r") do fid
+        if !isnothing(rng)
+            datasets = shuffle(rng, keys(fid["pmt_hits"]))
+        else
+            datasets = keys(fid["pmt_hits"])
+        end
+
+        
+        for grpn in datasets[1:nsel]
+            grp = fid["pmt_hits"][grpn]
+            hits = create_pmt_table(grp)
+            push!(all_hits, hits)
+        end
+
+       
+        for grpn in datasets
+            grp = fid["pmt_hits"][grpn]
+
+            if length(grp) < 5
+                continue
+            end
+
+            att_d = Dict(attrs(grp))
+            att_d["nhits"] = length(grp)
+            att_d["hittime_mean"] = mean(grp[:, 1])
+            att_d["hittime_std"] = std(grp[:, 1])
+
+            push!(attr_dicts, att_d)        
+        end        
+    end
+
+    hits_df = reduce(vcat, all_hits)
+    tres = hits_df[:, :tres]
+    nhits = hits_df[:, :nhits]
+    return tres, nhits, preproc_labels(hits_df), DataFrame(attr_dicts)
+
+end
+
+fname = joinpath(@__DIR__, "../assets/photon_table_extended_2.hd5")
+rng = MersenneTwister(31338)
+nsel = 15000
+
+tres, nhits, cond_labels, ds_summary = read_hdf(fname, nsel, rng)
+
+
+function train_model!(loss, opt, train, test, pars, epochs, logger)
+    local train_loss
+    @progress for epoch in 1:epochs
+        total_loss = 0
+        for d in train
+            # d = d |> device
+            gs = gradient(pars) do
+                train_loss = loss(d)
+                return train_loss
+            end
+            total_loss += train_loss /
+            #=
+            with_logger(logger) do
+                @info "train" batch_loss=train_loss
+            end
+            =#
+            Flux.update!(opt, pars, gs)
+        end
+        total_loss /= numobs(train)
+
+        total_loss_test = 0
+        for d in test
+            total_loss_test += loss(d)
+        end
+        total_loss_test /= numobs(test)
+
+
+        with_logger(logger) do
+            @info "loss" train=total_loss test=total_loss_test
+            
+        end
+        println("Epoch: $epoch, Train: $total_loss Test: $total_loss_test")
+
+    end
+end
+
+
+
+summary_data = ds_summary[:, [:nhits, :hittime_mean, :hittime_std]]
+summary_labels = preproc_labels(ds_summary)
+
+device = gpu
+
+norm_sum_lab = SKPreprocessor("Normalizer")
+norm_targ = SKPreprocessor("Normalizer")
+
+tr_summary_labels = fit_transform!(norm_sum_lab, summary_labels) |> Matrix |> adjoint
+tr_summary_data = fit_transform!(norm_targ, summary_data) |> Matrix |> adjoint
+for compression in [2, 4, 8, 512]
+
+    pre_model = Chain(
+        Dense(ncol(summary_labels) => 512, relu),
+        Dense(512 => compression, relu),
+        Dense(compression => 512, relu),
+        Dense(512 => ncol(summary_data))
+        ) |> device
+
+
+    mse_loss(x) = Flux.Losses.mse(pre_model(x[:label]), x[:data])
+
+    data = DataLoader(
+        (data=tr_summary_data, label=tr_summary_labels),
+        batchsize=100,
+        shuffle=true,
+        rng=rng)
+
+
+    pars = Flux.params(pre_model)
+    epochs = 150
+    lg = TBLogger("tensorboard_logs/pre_model_comp_$compression", tb_overwrite)
+    opt = Flux.Optimise.Adam(0.001)
+    train_model!(mse_loss, opt, data, pars, epochs, lg)
+end
+
+
 
 catf = CatFeatureSelector()
 ohe = OneHotEncoder()
@@ -87,7 +205,8 @@ function RQNormFlow(K, B, hidden_structure)
     for ix in 2:length(hidden_structure)
         push!(model, Dense(hidden_structure[ix-1] => hidden_structure[ix], relu))
     end
-    push!(model, Dense(hidden_structure[end] => 3 * K))
+    # 3 K -1 for spline, 1 for shift, 1 for scale, 1 for log-expectation
+    push!(model, Dense(hidden_structure[end] => 3 * K + 2))
 
     return RQNormFlow(Chain(model...), K, B)
 end
@@ -99,104 +218,104 @@ function (m::RQNormFlow)(x, cond)
 
     base = Normal()
     params = m.embedding(Float64.(cond))
+
     
-    res = CUDA.@allowscalar @inbounds [
+    logpdf_eval = CUDA.@allowscalar @inbounds [
         logpdf(
             transformed(
                 base,
-                Bijectors.Scale(sigmoid(p[end]) * 50) ∘ 
-                Bijectors.RationalQuadraticSpline(
+                Bijectors.Scale(sigmoid(p[end-1]) * 50)
+                ∘ Bijectors.Shift(p[end-2]) 
+                ∘ Bijectors.RationalQuadraticSpline(
                     p[1:m.K],
                     p[m.K+1:2*m.K],
-                    p[2*m.K+1:end-1],
+                    p[2*m.K+1:end-3],
                     m.B
                 )),
             ax)
         for (p, ax) in zip(eachcol(params), x)
     ]
 
-    return res
+    log_expec = params[end, :]
+    
+    return logpdf_eval, log_expec
 
-end
-
-function train_model!(loss, data, pars, epochs, logger)
-    local train_loss
-    @progress for epoch in 1:epochs
-        total_loss = 0
-        for d in data
-            gs = gradient(pars) do
-                train_loss = loss(d)
-                return train_loss
-            end
-            total_loss += train_loss
-            with_logger(logger) do
-                @info "train" batch_loss=train_loss
-            end
-            Flux.update!(opt, pars, gs)
-        end
-        total_loss /= length(data)
-        with_logger(logger) do
-            @info "train" loss=total_loss
-        end
-        println("Epoch: $epoch, Loss: $total_loss")
-
-    end
 end
 
 
 B = 5
 K = 15
-
-
-
 device = cpu
 
-size(tres)
-size(tr_cond_labels)
 
+train_data, test_data = splitobs((tres=tres, label=tr_cond_labels, nhits=nhits); at=0.85)
 
-data = DataLoader(
-    (tres=tres |> device, label=tr_cond_labels |> device),
-    batchsize=5000,
+train_loader = DataLoader(
+    train_data,
+    batchsize=3000,
     shuffle=true,
     rng=rng)
+
+test_loader = DataLoader(
+    test_data,
+    batchsize=10000,
+    shuffle=true,
+    rng=rng)
+
+
 rq_layer = RQNormFlow(K, B, [512, 512]) |> device
+function combined_loss(x)
+    logpdf_eval, log_expec = rq_layer(x[:tres], x[:label])
 
+    # poisson: log(exp(-lambda) * lambda^k)
+    poiss_f = -exp.(log_expec) .+ x[:nhits] .* log_expec .-loggamma.(x[:nhits] .+1)
 
-loss(x) = -sum(rq_layer(x[:tres], x[:label]))
+    return -sum(logpdf_eval) - sum(poiss_f) 
+end 
 
 pars = Flux.params(rq_layer)
 opt = Flux.Optimise.Adam(0.001)
-epochs = 10
+epochs = 50
 
-lg = TBLogger("tensorboard_logs/run", tb_overwrite)
-
-
-train_model!(loss, data, pars, epochs, lg)
-
-plot_tres, plot_labels = preproc(create_pmt_table(fid["pmt_hits"]["dataset_800"]))
+lg = TBLogger("tensorboard_logs/nflow", tb_overwrite)
 
 
-combine(groupby(plot_labels, :pmt_id), nrow => :n)
+train_model!(combined_loss, opt, train_loader, test_loader, pars, epochs, lg)
+h5open(fname, "r") do fid
+    df = create_pmt_table(fid["pmt_hits"]["dataset_10700"])
+    plot_tres = df[:, :tres]
+    plot_labels = preproc_labels(df)
+    @show combine(groupby(plot_labels, :pmt_id), nrow => :n)
+
+    n_per_pmt = combine(groupby(plot_labels, :pmt_id), nrow => :n)
+    max_i = argmax(n_per_pmt[:, :n])
 
 
-pmt_plot = 12
-mask = plot_labels.pmt_id.==pmt_plot
+    pmt_plot = n_per_pmt[max_i, :pmt_id]
+    mask = plot_labels.pmt_id.==pmt_plot
 
-plot_tres_m = plot_tres[mask]
-plot_labels_m = plot_labels[mask, :]
+    plot_tres_m = plot_tres[mask]
+    plot_labels_m = plot_labels[mask, :]
 
-plot_labels_m_tf = AutoMLPipeline.transform(traf, plot_labels_m)
+    plot_labels_m_tf = AutoMLPipeline.transform(traf, plot_labels_m)
 
-t_plot = -5:0.1:50
-l_plot = repeat(Vector(plot_labels_m_tf[1, :]), 1, length(t_plot))
+    t_plot = -5:0.1:100
+    l_plot = repeat(Vector(plot_labels_m_tf[1, :]), 1, length(t_plot))
 
-fig = Figure()
+    fig = Figure()
+    log_pdf_eval, log_expec = rq_layer(t_plot, l_plot)
+    lines(fig[1, 1], t_plot, exp.(log_pdf_eval))
+    hist!(fig[1, 1], plot_tres[mask], bins=-5:3:100, normalization=:pdf)
 
-lines(fig[1, 1], t_plot, exp.(rq_layer(t_plot, l_plot)))
+    lines(fig[1, 2], t_plot, exp.(log_pdf_eval) .* exp.(log_expec))
+    hist!(fig[1, 2], plot_tres[mask], bins=-5:3:100)
 
-hist!(fig[1, 1], plot_tres[mask], bins=-5:0.5:50, normalization=:pdf)
-fig
+    fig
+end
+
+
+
+
 
 
 
--- scripts/rq_spline_cuda.jl
+++ scripts/rq_spline_cuda.jl
@@ -39,37 +39,39 @@ function rqs_univariate(widths, heights, derivatives, x::Real)
 end
 
 
-function rqs_univariate(widths, heights, derivatives, x::CuVector{<:Real})
+function rqs_univariate(widths, heights, derivatives, x::AbstractArray{<:Real})
     T = promote_type(eltype(widths), eltype(heights), eltype(derivatives), eltype(x))
 
 
-    output = CUDA.ones(T, size(x))
+    output = similar(x)
+
     outmask = (x .≤ -widths[:, end]) .|| (x .≥ widths[:, end])
-    output[outmask] .*= x[outmask]
+    output[outmask] .= one(T)
 
-    inmask = .!outmask
-    widths_in = widths[inmask]
-    heights_in = heights[inmask]
-    x_in = x[inmask]
+    inmask = @. !outmask
+    widths_in = @view widths[inmask, :]
+    heights_in = @view heights[inmask, :]
+    devs_in = @view derivatives[inmask, :]
+    x_in = @view x[inmask]
 
-    K = size(widths_in, 1)
-
+    @show widths_in
     # Find which bin `x` is in; subtract 1 because `searchsortedfirst` returns idx of ≥ not ≤
-    k = searchsortedfirst.(widths_in, x_in) .- 1
+    k = searchsortedfirst.(eachrow(widths_in), x_in) .- 1
 
+    @show k
     # Width
     # If k == 0 then we should put it in the bin `[-B, widths_in[1]]`
     
     mask = k .!= 0
 
     w_k = -widths_in[:, end]
-    w_k[mask] = widths_in[k]
+    w_k[mask] = widths_in[:, k]
     
     w = widths_in[:, k .+ 1] .- w_k
 
     # Slope    
     h_k = -heights_in[:, end]
-    h_k[mask] = heights_in[k]
+    h_k[mask] = heights_in[:, k]
 
     Δy = heights_in[k .+ 1] .- h_k
 
@@ -79,16 +81,29 @@ function rqs_univariate(widths, heights, derivatives, x::CuVector{<:Real})
     # Derivatives at knot-points
     # Note that we have (K - 1) knot-points, not K
 
-    d_k = ones(T, size(x))
-    d_k[mask] = derivatives[:, k]
+    d_k = ones(T, size(x_in))
+    d_k[mask] = devs_in[:, k]
 
-    d_k = (k == 0) ? one(T) : derivatives[k]
-    d_kplus1 = (k == K - 1) ? one(T) : derivatives[k + 1]
+    K = size(widths_in, 1)
+    mask2 = k .!= (K-1)
+    d_kplus1 = ones(T, size(x_in))
+    d_kplus1[mask2] = devs_in[:, k .+ 1]
 
     # Eq. (14)
-    numerator = Δy * (s * ξ^2 + d_k * ξ * (1 - ξ))
-    denominator = s + (d_kplus1 + d_k - 2s) * ξ * (1 - ξ)
-    g = h_k + numerator / denominator
+    numerator = @. Δy * (s * ξ^2 + d_k * ξ * (1 - ξ))
+    denominator = @. s + (d_kplus1 + d_k - 2s) * ξ * (1 - ξ)
+    g = @. h_k + numerator / denominator
 
-    return g
+    output[inmask] = g
+
+    return output
 end
+
+h = randn((2, 10))
+w = randn((2, 10))
+d = randn((2, 9))
+x = randn(2)
+
+rqs_univariate(w, h, d, x)
+
+rqs_univariate(w[1, :], h[1, :], d[1, :], x[1])
