09347fa1862249b33991f0502507e27fe55b1f09
Christian Haack <christian.haack@tum.de>
2022-12-08 22:35:25 +0100

    updates

--- scripts/fisher_extended_casc_model.jl
+++ scripts/fisher_extended_casc_model.jl
@@ -6,19 +6,22 @@ using StaticArrays
 using BSON: @save, @load
 using BSON
 using CairoMakie
+using Rotations
+using LinearAlgebra
+using DataFrames
 model_path = joinpath(@__DIR__, "../assets/rq_spline_model.bson")
 
 @load model_path model hparams opt tf_dict
 
-pos = SA[0.0, 20.0, 0.0]
-dir_theta = deg2rad(20)
-dir_phi = deg2rad(50)
+pos = SA[0.0f0, 20.0f0, 10.0f0]
+dir_theta = deg2rad(20f0)
+dir_phi = deg2rad(50f0)
 dir = sph_to_cart(dir_theta, dir_phi)
 
 pmt_area = Float32((75e-3 / 2)^2 * π)
 target_radius = 0.21f0
 
-p = Particle(pos, dir, 0.0, 1E5, PEPlus)
+p = Particle(pos, dir, 0.0f0, Float32(1E5), PEMinus)
 target = MultiPMTDetector(
     @SVector[0.0f0, 0.0f0, 0.0f0],
     target_radius,
@@ -33,7 +36,15 @@ spectrum = CherenkovSpectrum(wl_range, 30, medium)
 
 source = ExtendedCherenkovEmitter(p, medium, wl_range)
 
-photons = PhotonPropSetup([p], [target], medium, spectrum)
+photon_setup = PhotonPropSetup([source], [target], medium, spectrum)
+photons = propagate_photons(photon_setup) 
+
+calc_total_weight!(photons, photon_setup)
+calc_time_residual!(photons, photon_setup)
+
+rot = RotMatrix3(I)
+
+hits = make_hits_from_photons(photons, photon_setup, rot)
 
 
 input = calc_flow_inputs([p], [target], tf_dict)
@@ -43,17 +54,32 @@ times = -10:1:100
 
 log_pdf, log_expec = model(repeat(times, size(input, 2)), repeat(input, inner=(1, length(times))), true)
 log_pdf = reshape(log_pdf, length(times), size(input, 2),)
+log_expec = reshape(log_expec, length(times), size(input, 2),)
 
-fig = Figure()
+
+combine(groupby(hits, :pmt_id), nrow=>:nrow)
+
+exp.(log_expec)
+
+size(log_pdf)
+
+exp.(log_pdf[:, 1])
+begin
+fig = Figure(resolution = (1000, 700))
 ga = fig[1, 1] = GridLayout(4, 4)
 li = CartesianIndices((4, 4))
 for i in 1:16
     row, col = divrem(i - 1, 4)
-    lines(ga[col+1, row+1], times, exp.(log_pdf[:, i]))
+    mask = hits[:, :pmt_id] .== i
+    ax = Axis(ga[col+1, row+1], xlabel="Time Residual(ns)", ylabel="Photons / time", title="PMT $i")
+    hist!(ax, hits[mask, :tres], bins=-10:5:100, weights=hits[mask, :total_weight], color=:orange, normalization=:density)
+    lines!(ax, times, exp.(log_pdf[:, i] + log_expec[:, i]))
+
+    @show sum(hits[mask, :total_weight]), exp.( log_expec[1, i])
 end
 
 fig
-li[2]
+end
 
 lines()
 lines(times, exp.(log_pdf[:, 2]))
