e7d1ae822f0b7faecb2c44c0c8a31235409b4c2f
Christian Haack <christian.haack@tum.de>
2022-12-09 09:26:42 +0100

    updates

--- scripts/train_time_surrogate.jl
+++ scripts/train_time_surrogate.jl
@@ -17,81 +17,40 @@ using BSON: @save, @load
 
 
 fnames = [
-    joinpath(@__DIR__, "../assets/photon_table_extended_2.hd5"),
-    joinpath(@__DIR__, "../assets/photon_table_extended_3.hd5"),
-    joinpath(@__DIR__, "../assets/photon_table_extended_4.hd5"),
-    joinpath(@__DIR__, "../assets/photon_table_extended_5.hd5")
-    ]
+    joinpath(@__DIR__, "../data/photon_table_extended_2.hd5"),
+    joinpath(@__DIR__, "../data/photon_table_extended_3.hd5"),
+    joinpath(@__DIR__, "../data/photon_table_extended_4.hd5"),
+    joinpath(@__DIR__, "../data/photon_table_extended_5.hd5"),
+    joinpath(@__DIR__, "../data/photon_table_extended_6.hd5")
+]
 
 
 rng = MersenneTwister(31338)
 nsel_frac = 0.1
-tres, nhits, cond_labels = read_hdf(fnames, nsel_frac, rng)
-tr_cond_labels, traf = fit_trafo_pipeline(cond_labels)
-
-function run_hyperopt(data)
-    inner_opt = BOHB(dims=[
-        Hyperopt.Continuous(),
-        Hyperopt.Continuous(),
-        Hyperopt.Continuous(),
-        Hyperopt.Continuous()]
-        )
+tres, nhits, cond_labels, tf_dict = read_hdf(fnames, nsel_frac, rng)
 
 
-    local model_seed = 1
-    
-    bohb = @hyperopt for resources=50,
-        sampler=Hyperband(R=50, η=3, inner=inner_opt),
-        batch_size = [1000, 2000, 5000, 10000],
-        lr = 10 .^ (-3:0.5:-1.5),
-        mlp_layer_size = LinRange(128, 1024, 5),
-        dropout = LinRange(0., 0.3, 20)
+hob = @hyperopt for i = 100,
+    sampler = CLHSampler(dims=[Continuous(), Categorical(4), Continuous(), Continuous(), Categorical(4)]),
+    lr = 10 .^ LinRange(-4, -2, 100),
+    mlp_layer_size = [256, 512, 768, 1024],
+    dropout = LinRange(0, 0.5, 100),
+    K = LinRange(5, 15, 11),
+    batch_size = [512, 1024, 2048, 4096]
 
-        epochs = Int(ceil(resources*5))
-
-
-        if state !== nothing
-            println("Continuing training with epochs=$epochs")
-            model_path = joinpath(@__DIR__, "../assets/rq_spline_model_hopt_$(state[1]).bson")
-
-            @load model_path model hparams opt
-
-            train_loader, test_loader = setup_dataloaders(data, hparams)
-
-            device = gpu
-            logdir = joinpath(@__DIR__, "../../tensorboard_logs/RQNormFlowHyperopt")
-            lg = TBLogger(logdir)
-            model, model_loss = train_model!(opt, train_loader, test_loader, model, log_likelihood_with_poisson, hparams, lg, device, true)
-
-            return model_loss, (hparams.seed, )
-            
-        else
-            println("New model with epochs=$epochs")
-            model, model_loss, hparams, opt = train_time_expectation_model(
-                data,
-                true,
-                true,
-                epochs=epochs,
-                lr=lr,
-                mlp_layer_size=Int(mlp_layer_size),
-                mlp_layers=2,
-                dropout=dropout,
-                non_linearity=:relu,
-                batch_size=batch_size,
-                seed=model_seed)
-
-            model_path = joinpath(@__DIR__, "../assets/rq_spline_model_hopt_$(model_seed).bson")
-            @save model_path model hparams opt
-            model_seed += 1
-
-            return model_loss, (hparams.seed, )
-
-        end
-    end
-
-    return bohb
+    model, model_loss, hparams, opt = train_time_expectation_model(
+        (tres=tres, label=cond_labels, nhits=nhits),
+        true,
+        true,
+        K=Int(K),
+        epochs=100,
+        lr=lr,
+        mlp_layer_size=mlp_layer_size,
+        mlp_layers=2,
+        dropout=dropout,
+        non_linearity=:relu,
+        batch_size=batch_size,
+        seed=1,)
 end
 
-bohb = run_hyperopt((tres=tres, label=tr_cond_labels, nhits=nhits))
-
-
+print(hob)
