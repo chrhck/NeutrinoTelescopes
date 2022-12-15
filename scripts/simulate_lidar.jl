82b1545662fb468f32f1780c0486ac559adff36f
Christian Haack <christian.haack@tum.de>
2022-12-15 11:33:08 +0100

    updates

--- scripts/simulate_lidar.jl
+++ scripts/simulate_lidar.jl
@@ -7,6 +7,10 @@ using LinearAlgebra
 using ProgressLogging
 using Parquet
 using Unitful
+using TerminalLoggers
+using Logging
+
+global_logger(TerminalLogger(right_justify=120))
 
 medium = make_cascadia_medium_properties(0.99f0)
 mono_spectrum = Monochromatic(450.0f0)
@@ -19,27 +23,12 @@ target = RectangularDetector(
 )
 
 
-laser_pos = @SVector[0.0f0, 0.05f0, 0f0]
-# Meet laser beam at 20m distance
-
-# TODO ISEC ASSUMES normal IS e_z
-function intersect_rect(photons, normal, center, dx, dy)
-    dirnormal = dot.(photons[:, :direction], Ref(normal))
-    d = dot.(Ref(center) .- photons[:, :position], Ref(normal)) ./ dirnormal
-
-    isec_point = reduce(hcat, photons[:, :position] .+ photons[:, :direction] .* d)
-    isec = (abs.(isec_point[1, :] .- center[1]) .< dx) .& (abs.((isec_point[2, :] .- center[2])) .< dy)
-    isec .&= (dirnormal .< 0)
-
-    return isec
-end
-
-
+laser_pos = @SVector[0.0f0, 0.05f0, 0.0f0]
 
 all_photons = []
 @progress for d in [20, 40, 60, 100, 150]
 
-    beam_dir = @SVector[0f0, laser_pos[2] / (d - laser_pos[3]), 1f0]
+    beam_dir = @SVector[0.0f0, laser_pos[2] / (d - laser_pos[3]), 1.0f0]
     beam_dir = beam_dir ./ norm(beam_dir)
     beam_divergence = Float32(asin(2E-3 / 5))
 
@@ -52,7 +41,7 @@ all_photons = []
         Int64(1E11)
     )
 
-    @progress for i in 1:10
+    @progress for i in 1:300
         setup = PhotonPropSetup(prop_source_pencil_beam, target, medium, mono_spectrum, Int64(i))
         photons = propagate_photons(setup)
         calc_total_weight!(photons, setup)
@@ -60,36 +49,36 @@ all_photons = []
         push!(all_photons, photons)
     end
 
-    
+
 end
 
 photons = reduce(vcat, all_photons)
 
 incident_angle = acos.(dot.(Ref(@SVector[0, 0, 1]), -photons[:, :direction]))
+
+photons[:, :incident_angle] = incident_angle
 mask = incident_angle .< deg2rad(0.4)
 photons[:, :fov] = mask
 
-photons_sav = photons[photons[:, :fov], [:time, :d, :abs_weight]]
-write_parquet("lidar_photons.parquet", photons_sav)
+write_parquet("lidar_photons.parquet", photons)
 
 #=
-photons[photons[:, :fov_and_hit], :position]
+photons_sav = DataFrame(read_parquet("data/lidar_photons.parquet"))
+photons_sav
 
 
 begin
     fig = Figure()
     ga = fig[1, 1] = GridLayout(3, 3)
     li = CartesianIndices((3, 3))
-    bins = 0:50:1500
-    for (i, (key, grp)) in enumerate(pairs(groupby(photons, :d)))
+    bins = 0:30:1500
+    for (i, (key, grp)) in enumerate(pairs(groupby(photons_sav, :d)))
         ax = Axis(ga[Tuple(li[i])...], xlabel="Time (ns)", ylabel="PDF", yscale=log10, limits=(-10, 1510, 1E-7, 1),
-                 title="Focus distance: $(key[1])m")
-        m = grp[:, :fov_and_hit]
-        #m = trues(nrow(grp))
+            title="Focus distance: $(key[1])m")
 
-        hist!(ax, grp[m, :time], weights=grp[m, :total_weight], bins=bins, normalization=:pdf, fillto = 1E-7)
+
+        hist!(ax, Float64.(grp[:, :time]), weights=Float64.(grp[:, :abs_weight]), bins=bins, normalization=:pdf, fillto=1E-7)
     end
     fig
 end
-
 =#
